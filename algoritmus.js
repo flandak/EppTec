f93e259f9cf81730ffd50e7d284ad202392c831e
Jakub Flanderka <flandak@gmail.com>
2023-02-28 17:21:07 +0100

    changed names

--- algoritmus.js
+++ algoritmus.js
@@ -1,16 +1,16 @@
 let pole = [5, 10, 15, 20, 25, 30];
 
-const FILTR_NAZEV = "--filtr";
-const PRAVIDLO_NAZEV = "--pravidlo";
+const FILTR = "--filtr";
+const PRAVIDLO = "--pravidlo";
 
 // získání hodnoty parametru pro filtr a pravidlo z příkazové řádky
 let filtrHodnota = null;
 let pravidloHodnota = null;
 process.argv.forEach((argument, index) => {
-  if (argument === FILTR_NAZEV && index < process.argv.length - 1) {
+  if (argument === FILTR && index < process.argv.length - 1) {
     filtrHodnota = parseInt(process.argv[index + 1]);
   }
-  if (argument === PRAVIDLO_NAZEV && index < process.argv.length - 1) {
+  if (argument === PRAVIDLO && index < process.argv.length - 1) {
     pravidloHodnota = process.argv[index + 1];
   }
 });
